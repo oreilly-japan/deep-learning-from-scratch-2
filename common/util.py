350e02110a3ba9c648b172f20f5e5e1d6a30701c
Koki Saitoh <koki0702@gmail.com>
2018-04-19 10:35:12 +0900

    Rename idx to id

--- common/util.py
+++ common/util.py
@@ -104,18 +104,18 @@ def create_co_matrix(corpus, vocab_size, window_size=1):
     corpus_size = len(corpus)
     co_matrix = np.zeros((vocab_size, vocab_size), dtype=np.int32)
 
-    for idx, word_idx in enumerate(corpus):
+    for idx, word_id in enumerate(corpus):
         for i in range(1, window_size + 1):
             left_idx = idx - i
             right_idx = idx + i
 
             if left_idx >= 0:
-                left_word_idx = corpus[left_idx]
-                co_matrix[word_idx, left_word_idx] += 1
+                left_word_id = corpus[left_idx]
+                co_matrix[word_id, left_word_id] += 1
 
             if right_idx < corpus_size:
-                right_word_idx = corpus[right_idx]
-                co_matrix[word_idx, right_word_idx] += 1
+                right_word_id = corpus[right_idx]
+                co_matrix[word_id, right_word_id] += 1
 
     return co_matrix
 
