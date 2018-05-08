1585b7465eda0bd7c9c0fcad54c0b7b3b11b47b0
Koki Saitoh <koki0702@gmail.com>
2018-05-08 09:43:40 +0900

    Minor fix

--- ch02/show_ptb.py
+++ ch02/show_ptb.py
@@ -4,15 +4,15 @@ sys.path.append('..')
 from dataset import ptb
 
 
-corpus, word_to_id, id_to_word = ptb.load_data("train")
+corpus, word_to_id, id_to_word = ptb.load_data('train')
 
-print("corpus size:", len(corpus))
-print("corpus[:30]:", corpus[:30])
-
-print("\nid_to_word[0]:", id_to_word[0])
-print("id_to_word[1]:", id_to_word[1])
-print("id_to_word[2]:", id_to_word[2])
-
-print("\nword_to_id['car']:", word_to_id["car"])
-print("word_to_id['happy']:", word_to_id["happy"])
-print("word_to_id['lexus']:", word_to_id["lexus"])
+print('corpus size:', len(corpus))
+print('corpus[:30]:', corpus[:30])
+print()
+print('id_to_word[0]:', id_to_word[0])
+print('id_to_word[1]:', id_to_word[1])
+print('id_to_word[2]:', id_to_word[2])
+print()
+print("word_to_id['car']:", word_to_id['car'])
+print("word_to_id['happy']:", word_to_id['happy'])
+print("word_to_id['lexus']:", word_to_id['lexus'])
