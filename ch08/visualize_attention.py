60a0824ced85b60f49a57097c813a60ad4faf126
Koki Saitoh <koki0702@gmail.com>
2018-05-23 08:28:51 +0900

    Minor fix

--- ch08/visualize_attention.py
+++ ch08/visualize_attention.py
@@ -1,7 +1,7 @@
 # coding: utf-8
 import sys
-sys.path.append('..')  # 親ディレクトリのファイルをインポートするための設定
-import numpy as np`
+sys.path.append('..')
+import numpy as np
 from dataset import sequence
 import matplotlib.pyplot as plt
 from attention_seq2seq import AttentionSeq2seq
