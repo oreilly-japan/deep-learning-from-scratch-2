3efb9ca4a09aebbf9d46189a71fa327c9e22cd80
Koki Saitoh <koki0702@gmail.com>
2018-06-20 10:55:50 +0900

    Change quotation

--- ch07/show_addition_dataset.py
+++ ch07/show_addition_dataset.py
@@ -5,7 +5,7 @@ from dataset import sequence
 
 
 (x_train, t_train), (x_test, t_test) = \
-    sequence.load_data("addition.txt", seed=1984)
+    sequence.load_data('addition.txt', seed=1984)
 char_to_id, id_to_char = sequence.get_vocab()
 
 print(x_train.shape, t_train.shape)
@@ -18,7 +18,7 @@ print(t_train[0])
 # [ 3  0  2  0  0 11  5]
 # [ 6  0 11  7  5]
 
-print("".join([id_to_char[c] for c in x_train[0]]))
-print("".join([id_to_char[c] for c in t_train[0]]))
+print(''.join([id_to_char[c] for c in x_train[0]]))
+print(''.join([id_to_char[c] for c in t_train[0]]))
 # 71+118
 # _189
