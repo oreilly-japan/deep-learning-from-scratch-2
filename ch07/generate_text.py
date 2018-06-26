2de3dc95da4a7aa99209487c17e70bd8a23a3d48
Koki Saitoh <koki0702@gmail.com>
2018-06-26 11:45:34 +0900

    Minor change

--- ch07/generate_text.py
+++ ch07/generate_text.py
@@ -4,6 +4,7 @@ sys.path.append('..')
 from rnnlm_gen import RnnlmGen
 from dataset import ptb
 
+
 corpus, word_to_id, id_to_word = ptb.load_data('train')
 vocab_size = len(word_to_id)
 corpus_size = len(corpus)
@@ -20,5 +21,4 @@ skip_ids = [word_to_id[w] for w in skip_words]
 word_ids = model.generate(start_id, skip_ids)
 txt = ' '.join([id_to_word[i] for i in word_ids])
 txt = txt.replace(' <eos>', '.\n')
-
 print(txt)
