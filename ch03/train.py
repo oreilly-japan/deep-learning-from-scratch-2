2c89e945698bf90dd40808318284c270f9173b14
Koki Saitoh <koki0702@gmail.com>
2018-06-26 10:54:23 +0900

    Remove method arg

--- ch03/train.py
+++ ch03/train.py
@@ -16,7 +16,7 @@ text = 'You say goodbye and I say hello.'
 corpus, word_to_id, id_to_word = preprocess(text)
 
 vocab_size = len(word_to_id)
-contexts, target = create_contexts_target(corpus, window_size=window_size)
+contexts, target = create_contexts_target(corpus, window_size)
 target = convert_one_hot(target, vocab_size)
 contexts = convert_one_hot(contexts, vocab_size)
 
