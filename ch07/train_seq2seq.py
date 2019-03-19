8aba942c4c20be0d90ace45d3b36b641c5afad3f
Wegra Lee <wegra.lee@gmail.com>
2019-03-19 19:51:38 +0900

    주석 번역

--- ch07/train_seq2seq.py
+++ ch07/train_seq2seq.py
@@ -11,17 +11,17 @@ from seq2seq import Seq2seq
 from peeky_seq2seq import PeekySeq2seq
 
 
-# データセットの読み込み
+# 데이터셋 읽기
 (x_train, t_train), (x_test, t_test) = sequence.load_data('addition.txt')
 char_to_id, id_to_char = sequence.get_vocab()
 
-# Reverse input? =================================================
+# 입력 반전 여부 설정 =============================================
 is_reverse = False  # True
 if is_reverse:
     x_train, x_test = x_train[:, ::-1], x_test[:, ::-1]
 # ================================================================
 
-# ハイパーパラメータの設定
+# 하이퍼파라미터 설정
 vocab_size = len(char_to_id)
 wordvec_size = 16
 hideen_size = 128
@@ -29,7 +29,7 @@ batch_size = 128
 max_epoch = 25
 max_grad = 5.0
 
-# Normal or Peeky? ==============================================
+# 일반 혹은 엿보기(Peeky) 설정 =====================================
 model = Seq2seq(vocab_size, wordvec_size, hideen_size)
 # model = PeekySeq2seq(vocab_size, wordvec_size, hideen_size)
 # ================================================================
@@ -50,13 +50,13 @@ for epoch in range(max_epoch):
 
     acc = float(correct_num) / len(x_test)
     acc_list.append(acc)
-    print('val acc %.3f%%' % (acc * 100))
+    print('정확도 %.3f%%' % (acc * 100))
 
-# グラフの描画
+# 그래프 그리기
 x = np.arange(len(acc_list))
 plt.plot(x, acc_list, marker='o')
-plt.xlabel('epochs')
-plt.ylabel('accuracy')
+plt.xlabel('에폭')
+plt.ylabel('정확도')
 plt.ylim(0, 1.0)
 plt.show()
 
