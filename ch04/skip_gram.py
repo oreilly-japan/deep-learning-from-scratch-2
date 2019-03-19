ecf1c0e3ef341de8957a6704383dd2ce52bd1eef
Wegra Lee <wegra.lee@gmail.com>
2019-03-19 19:01:16 +0900

    주석 번역

--- ch04/skip_gram.py
+++ ch04/skip_gram.py
@@ -1,6 +1,6 @@
 # coding: utf-8
 import sys
-sys.path.append('..')  # 親ディレクトリのファイルをインポートするための設定
+sys.path.append('..')  # 부모 디렉터리의 파일을 가져올 수 있도록 설정
 from common.layers import *
 from ch04.negative_sampling_layer import NegativeSamplingLoss
 
@@ -10,25 +10,25 @@ class SkipGram:
         V, H = vocab_size, hidden_size
         rn = np.random.randn
 
-        # 重みの初期化
+        # 가중치 초기화
         W_in = 0.01 * rn(V, H).astype('f')
         W_out = 0.01 * rn(V, H).astype('f')
 
-        # レイヤの生成
+        # 계층 생성
         self.in_layer = Embedding(W_in)
         self.loss_layers = []
         for i in range(2 * window_size):
             layer = NegativeSamplingLoss(W_out, corpus, power=0.75, sample_size=5)
             self.loss_layers.append(layer)
 
-        # すべての重みと勾配をリストにまとめる
+        # 모든 가중치와 기울기를 리스트에 모은다.
         layers = [self.in_layer] + self.loss_layers
         self.params, self.grads = [], []
         for layer in layers:
             self.params += layer.params
             self.grads += layer.grads
 
-        # メンバ変数に単語の分散表現を設定
+        # 인스턴스 변수에 단어의 분산 표현을 저장한다.
         self.word_vecs = W_in
 
     def forward(self, contexts, target):
