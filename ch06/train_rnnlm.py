ac720b969544b3b0ec5e9094277f84f68493730f
Wegra Lee <wegra.lee@gmail.com>
2019-03-19 19:20:37 +0900

    주석 저장

--- ch06/train_rnnlm.py
+++ ch06/train_rnnlm.py
@@ -11,33 +11,33 @@ from rnnlm import Rnnlm
 # ハイパーパラメータの設定
 batch_size = 20
 wordvec_size = 100
-hidden_size = 100  # RNNの隠れ状態ベクトルの要素数
-time_size = 35  # RNNを展開するサイズ
+hidden_size = 100  # RNN의 은닉 상태 벡터의 원소 수
+time_size = 35     # RNN을 펼치는 크기
 lr = 20.0
 max_epoch = 4
 max_grad = 0.25
 
-# 学習データの読み込み
+# 학습 데이터 읽기
 corpus, word_to_id, id_to_word = ptb.load_data('train')
 corpus_test, _, _ = ptb.load_data('test')
 vocab_size = len(word_to_id)
 xs = corpus[:-1]
 ts = corpus[1:]
 
-# モデルの生成
+# 모델 생성
 model = Rnnlm(vocab_size, wordvec_size, hidden_size)
 optimizer = SGD(lr)
 trainer = RnnlmTrainer(model, optimizer)
 
-# 勾配クリッピングを適用して学習
+# 기울기 클리핑을 적용하여 학습
 trainer.fit(xs, ts, max_epoch, batch_size, time_size, max_grad,
             eval_interval=20)
 trainer.plot(ylim=(0, 500))
 
-# テストデータで評価
+# 테스트 데이터로 평가
 model.reset_state()
 ppl_test = eval_perplexity(model, corpus_test)
 print('test perplexity: ', ppl_test)
 
-# パラメータの保存
+# 매개변수 저장
 model.save_params()
