775aadb61e8831be9d5b14615045c89588d61566
Wegra Lee <wegra.lee@gmail.com>
2019-03-19 18:53:29 +0900

    주석 번역

--- ch04/cbow.py
+++ ch04/cbow.py
@@ -10,25 +10,25 @@ class CBOW:
     def __init__(self, vocab_size, hidden_size, window_size, corpus):
         V, H = vocab_size, hidden_size
 
-        # 重みの初期化
+        # 가중치 초기화
         W_in = 0.01 * np.random.randn(V, H).astype('f')
         W_out = 0.01 * np.random.randn(V, H).astype('f')
 
-        # レイヤの生成
+        # 계층 생성
         self.in_layers = []
         for i in range(2 * window_size):
-            layer = Embedding(W_in)  # Embeddingレイヤを使用
+            layer = Embedding(W_in)  # Embedding 계층 사용
             self.in_layers.append(layer)
         self.ns_loss = NegativeSamplingLoss(W_out, corpus, power=0.75, sample_size=5)
 
-        # すべての重みと勾配をリストにまとめる
+        # 모든 가중치와 기울기를 배열에 모은다.
         layers = self.in_layers + [self.ns_loss]
         self.params, self.grads = [], []
         for layer in layers:
             self.params += layer.params
             self.grads += layer.grads
 
-        # メンバ変数に単語の分散表現を設定
+        # 인스턴스 변수에 단어의 분산 표현을 저장한다.
         self.word_vecs = W_in
 
     def forward(self, contexts, target):
