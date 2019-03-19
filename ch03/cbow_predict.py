14bb3a5073b291d05e46ce98424b1c701f8e66ba
Wegra Lee <wegra.lee@gmail.com>
2019-03-19 18:49:39 +0900

    주석 번역

--- ch03/cbow_predict.py
+++ ch03/cbow_predict.py
@@ -5,20 +5,20 @@ import numpy as np
 from common.layers import MatMul
 
 
-# サンプルのコンテキストデータ
+# 샘플 맥락 데이터
 c0 = np.array([[1, 0, 0, 0, 0, 0, 0]])
 c1 = np.array([[0, 0, 1, 0, 0, 0, 0]])
 
-# 重みの初期化
+# 가중치 초기화
 W_in = np.random.randn(7, 3)
 W_out = np.random.randn(3, 7)
 
-# レイヤの生成
+# 계층 생성
 in_layer0 = MatMul(W_in)
 in_layer1 = MatMul(W_in)
 out_layer = MatMul(W_out)
 
-# 順伝搬
+# 순전파
 h0 = in_layer0.forward(c0)
 h1 = in_layer1.forward(c1)
 h = 0.5 * (h0 + h1)
