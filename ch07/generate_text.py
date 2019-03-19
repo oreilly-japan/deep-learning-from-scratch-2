95a8c019bd9dbf37d399bf4b14cef4bf98979133
Wegra Lee <wegra.lee@gmail.com>
2019-03-19 19:46:15 +0900

    주석 번역

--- ch07/generate_text.py
+++ ch07/generate_text.py
@@ -12,12 +12,12 @@ corpus_size = len(corpus)
 model = RnnlmGen()
 model.load_params('../ch06/Rnnlm.pkl')
 
-# start文字とskip文字の設定
+# start 문자와 skip 문자 설정
 start_word = 'you'
 start_id = word_to_id[start_word]
 skip_words = ['N', '<unk>', '$']
 skip_ids = [word_to_id[w] for w in skip_words]
-# 文章生成
+# 문장 생성
 word_ids = model.generate(start_id, skip_ids)
 txt = ' '.join([id_to_word[i] for i in word_ids])
 txt = txt.replace(' <eos>', '.\n')
