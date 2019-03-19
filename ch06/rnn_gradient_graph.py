d7333cecda3ce55ed9ff2750833e5724792bc6b4
Wegra Lee <wegra.lee@gmail.com>
2019-03-19 19:16:24 +0900

    주석 번역

--- ch06/rnn_gradient_graph.py
+++ ch06/rnn_gradient_graph.py
@@ -3,13 +3,13 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 
-N = 2  # ミニバッチサイズ
-H = 3  # 隠れ状態ベクトルの次元数
-T = 20  # 時系列データの長さ
+N = 2   # 미니배치 크기
+H = 3   # 은닉 상태 벡터의 차원 수
+T = 20  # 시계열 데이터의 길이
 
 dh = np.ones((N, H))
 
-np.random.seed(3)
+np.random.seed(3) # 재현할 수 있도록 난수의 시드 고정
 
 Wh = np.random.randn(H, H)
 #Wh = np.random.randn(H, H) * 0.5
@@ -22,9 +22,9 @@ for t in range(T):
 
 print(norm_list)
 
-# グラフの描画
+# 그래프 그리기
 plt.plot(np.arange(len(norm_list)), norm_list)
 plt.xticks([0, 4, 9, 14, 19], [1, 5, 10, 15, 20])
-plt.xlabel('time step')
-plt.ylabel('norm')
+plt.xlabel('시간 크기(time step)')
+plt.ylabel('노름(norm)')
 plt.show()
