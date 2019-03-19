53e7bf61719609b2f121d6a7575bfdd655525664
Wegra Lee <wegra.lee@gmail.com>
2019-03-19 18:37:52 +0900

    주석 번역

--- ch01/forward_net.py
+++ ch01/forward_net.py
@@ -24,20 +24,20 @@ class TwoLayerNet:
     def __init__(self, input_size, hidden_size, output_size):
         I, H, O = input_size, hidden_size, output_size
 
-        # 重みとバイアスの初期化
+        # 가중치와 편향 초기화
         W1 = np.random.randn(I, H)
         b1 = np.random.randn(H)
         W2 = np.random.randn(H, O)
         b2 = np.random.randn(O)
 
-        # レイヤの生成
+        # 계층 생성
         self.layers = [
             Affine(W1, b1),
             Sigmoid(),
             Affine(W2, b2)
         ]
 
-        # すべての重みをリストにまとめる
+        # 모든 가중치를 리스트에 모은다.
         self.params = []
         for layer in self.layers:
             self.params += layer.params
