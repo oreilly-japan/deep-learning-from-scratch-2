3b6b9d93d0b7b59b60cf97035ccb0a80c735e0e0
Wegra Lee <wegra.lee@gmail.com>
2019-03-19 19:56:54 +0900

    주석 번역

--- dataset/spiral.py
+++ dataset/spiral.py
@@ -4,15 +4,15 @@ import numpy as np
 
 def load_data(seed=1984):
     np.random.seed(seed)
-    N = 100  # クラスごとのサンプル数
-    DIM = 2  # データの要素数
-    CLS_NUM = 3  # クラス数
+    N = 100  # 클래스당 샘플 수
+    DIM = 2  # 데어터 요소 수
+    CLS_NUM = 3  # 클래스 수
 
     x = np.zeros((N*CLS_NUM, DIM))
     t = np.zeros((N*CLS_NUM, CLS_NUM), dtype=np.int)
 
     for j in range(CLS_NUM):
-        for i in range(N):#N*j, N*(j+1)):
+        for i in range(N): # N*j, N*(j+1)):
             rate = i / N
             radius = 1.0*rate
             theta = j*4.0 + 4.0*rate + np.random.randn()*0.2
