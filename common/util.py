c8766da79aff58ca5acc3fa8a64c8d7cbbde5c9b
Koki Saitoh <koki0702@gmail.com>
2020-05-15 15:31:06 +0900

    Fix ppmi()'s DIV/0

--- common/util.py
+++ common/util.py
@@ -141,7 +141,7 @@ def ppmi(C, verbose=False, eps = 1e-8):
 
             if verbose:
                 cnt += 1
-                if cnt % (total//100) == 0:
+                if cnt % (total//100 + 1) == 0:
                     print('%.1f%% done' % (100*cnt/total))
     return M
 
